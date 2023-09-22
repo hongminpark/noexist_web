2ea5d40f0abbcfe187e1e379aa69d3232bdb5984
hongminpark <zhdals1026@gmail.com>
2023-09-22 16:02:40 +0900

    Move feed as component from page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import ModelFeed2 from "./ModelFeed";
+import ModelFeed2 from "../components/ModelFeed";
 
 export default function Home() {
   return (
